3dae015a3e8df59673ce41e80c22c63a515f2b97
Julio Sousa <juliosousajr@gmail.com>
2019-11-19 21:01:53 -0300

    alterando pagina inicial

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/assets')
 
 @app.route('/')
-def hello_world():
+def start():
     return render_template('index.html')
+
+# Definindo rotas
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('assets/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('assets/css', path)
+
+
+@app.route('/images/<path:path>')
+def send_images(path):
+    return send_from_directory('assets/images', path)
+
+@app.route('/fonts/<path:path>')
+def send_fonts(path):
+    return send_from_directory('assets/fonts', path)
 
 
 @app.route('/sobre/')
 def about():
+    print("Eulálio é feio!")
+    print(1+1)
     return render_template('sobre.html')
 
 if __name__ == '__main__':
